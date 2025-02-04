703992419773a9b3a5d3acc6856d0ab971cbdd02
kai <user@USERnoMac-mini.elecom>
2025-02-04 21:52:16 +0900

    fix: SPのスクロール問題修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Chat from "../components/Chat";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen sm:pb-6 gap-16">
+    <div className="flex flex-col min-h-dvh sm:min-h-screen sm:pb-6 gap-16">
       <main >
         <Chat />
       </main>
